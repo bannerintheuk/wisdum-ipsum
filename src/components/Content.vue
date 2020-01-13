e8a5a445d2d8f84f68b43b0e9bd95cd05d6e12ed
Bannerintheuk <bannerintheuk@gmail.com>
2020-01-13 14:31:17 +0000

    Added Content / Sidebar components

---
+++ src/components/Content.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    class="content w-full ml-4 p-4 h-full flex flex-col justify-center items-center bgOpaque rounded"
+  >
+    <img
+      alt="Thinking related Gif"
+      class="w-full"
+      :src="state.imageUrl"
+      v-if="state.fetching"
+    />
+    <img
+      alt="Game of Thrones wisdom Gif"
+      class="w-full"
+      src="/images/wisdom.webp"
+      v-if="
+        !state.fetching && state.text.length === 0 && state.quotes.length === 0
+      "
+    />
+    <textarea
+      v-if="!state.fetching && state.quotes.length > 0"
+      class="bgTransparent rounded text-sm flex-1 w-full h-full p-4 overflow-y-scroll resize-none"
+      :value="state.quotes"
+    />
+    <textarea
+      v-if="!state.fetching && !state.wrap && state.text.length > 0"
+      class="bgTransparent rounded text-sm flex-1 w-full h-full p-4 overflow-y-scroll resize-none"
+      :value="state.text"
+    />
+    <div
+      v-html="state.text"
+      v-if="!state.fetching && state.wrap && state.text.length > 0"
+      class="text-wrapper rounded bgTransparent text-sm flex-1 w-full h-full p-4 overflow-y-scroll resize-none"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Content"
+};
+</script>
+
+<style>
+.content {
+  min-width: 300px;
+}
+.bgOpaque {
+  background-color: rgba(255, 255, 255, 0.7);
+}
+.text-wrapper > p,
+div {
+  margin: 0.5rem 0;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="sidebar h-full bgOpaque p-4 rounded">
+    <div class="my-4"></div>
+    <div class="text-xl">Paragraphs</div>
+    <MinWords />
+    <NumberParagraphs />
+    <Wrap />
+    <div class="my-2"></div>
+    <GetTextButton />
+    <CopyTextButton />
+    <div class="h-4 border-b-2 border-gray-500"></div>
+    <div class="text-xl">Quotes</div>
+    <NumberQuotes />
+    <GetQuotesButton />
+    <CopyQuotesButton />
+  </div>
+</template>
+
+<script>
+import MinWords from "./MinWords";
+import NumberParagraphs from "./NumberParagraphs";
+import Wrap from "./Wrap";
+import GetTextButton from "./GetTextButton";
+import CopyTextButton from "./CopyTextButton";
+import NumberQuotes from "./NumberQuotes";
+import GetQuotesButton from "./GetQuotesButton";
+import CopyQuotesButton from "./CopyQuotesButton";
+export default {
+  name: "Sidebar",
+  components: {
+    MinWords,
+    NumberParagraphs,
+    Wrap,
+    GetTextButton,
+    CopyTextButton,
+    NumberQuotes,
+    GetQuotesButton,
+    CopyQuotesButton
+  }
+};
+</script>
+
+<style lang="postcss">
+.btn {
+  @apply h-8  rounded w-full p-1 text-white cursor-pointer;
+}
+.sidebar {
+  min-width: 230px;
+}
+.bgOpaque {
+  background-color: rgba(255, 255, 255, 0.7);
+}
+</style>
